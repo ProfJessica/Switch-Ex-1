a7f158c39503ccc5df5a213ff1a03c1f95227960
ProfJessica <jessica.sgarcez@lideres.alicerceedu.com.br>
2021-08-28 16:38:44 -0300

    Exercício Resolvido

--- src/switchexercicio1/SwitchExercicio1.java
+++ src/switchexercicio1/SwitchExercicio1.java
@@ -1,4 +1,5 @@
 package switchexercicio1;
+import java.util.Scanner;
 
 //Implemente em Java, um código que leia uma letra entre “a” e “d”.
 //Utilize uma estrutura switch-case em que cada caso imprime o nome
@@ -9,7 +10,28 @@ public class SwitchExercicio1 {
 
  
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite uma letra: ");
+        String letra = input.nextLine();
         
+        String fruta;
+        switch (letra) {
+            case "a":
+              fruta = "Maracujá";
+              break;
+            case "b":
+              fruta = "Maçã";
+              break;
+            case "c":
+              fruta = "Goiaba";
+              break;
+            case "d":
+              fruta = "Amora";
+              break;
+            default:
+              fruta = "Você cometeu um erro.";
+        }
+        System.out.println(fruta);
     }
     
 }

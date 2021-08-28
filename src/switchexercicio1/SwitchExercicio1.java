b6d71cba34ed703e6004bb4ea56d96f50144f785
ProfJessica <jessica.sgarcez@lideres.alicerceedu.com.br>
2021-08-28 14:25:57 -0300

    Enunciado

--- src/switchexercicio1/SwitchExercicio1.java
+++ src/switchexercicio1/SwitchExercicio1.java
@@ -1,9 +1,9 @@
 package switchexercicio1;
 
-// Implemente em Java, um código que leia uma letra entre “a” e “d”.
-// Utilize uma estrutura switch-case em que cada caso imprime o nome
-// de uma fruta diferente e o padrão mostre que o usuário cometeu um
-// erro.
+//Implemente em Java, um código que leia uma letra entre “a” e “d”.
+//Utilize uma estrutura switch-case em que cada caso imprime o nome
+//de uma fruta diferente e o padrão mostre que o usuário cometeu um
+//erro.
 
 public class SwitchExercicio1 {
 
